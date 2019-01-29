f158c09dc0dad22c3a7643bb41c4e6e2bda5e17e
MagicManFoli <magicmanfoli@gmail.com>
2019-01-29 23:10:42 +0100

    additional documentation

--- app.py
+++ app.py
@@ -22,16 +22,20 @@ def solve():
 
 # TODO: "/solved/<id>"
 # cache results in ring buffer or with max_time
+# return "still calculating" for keys(ids) not in solved but in pending
+# return 404 for keys not in solved or pending
 
 
 @app.route("/", methods=["GET"])
 def index():
+    # TODO: add description and index
     return f"Hello Flask!\n"
 
 
 @app.route("/about", methods=["GET"])
 def about():
-    return "https://github.com/ModischFabrications/CutSolver\n"
+    return 'Visit <a href="https://github.com/ModischFabrications/CutSolver">' \
+           'the repository</a> for further informations'
 
 
 if __name__ == "__main__":
