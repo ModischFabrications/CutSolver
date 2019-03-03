cdd360fe0ee00d27f44aea32d7252e09f5dc1edd
MagicManFoli <magicmanfoli@gmail.com>
2019-03-03 18:06:21 +0100

    try to make deployable on RPI

--- docker_start.sh
+++ docker_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run --name cutsolver -d -p 80:80 --rm cutsolver:latest
+docker run --name cutsolver --restart=always -d -p 80:80 --rm cutsolver:latest
 
 # -it: interactive (debugging)
 # -d: background (normal use)
