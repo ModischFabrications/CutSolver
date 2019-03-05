46757a88a511690f887f3452484a35b8770e7c10
MagicManFoli <magicmanfoli@gmail.com>
2019-03-05 19:36:15 +0100

    extend documentation & improve docker_start

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-docker run --name cutsolver --restart=always -d -p 80:80 --rm cutsolver:latest
-# docker run --name cutsolver -it -p 80:80 --rm cutsolver:latest
+docker container rm cutsolver
+docker run --name cutsolver --restart=always -d -p 80:80 cutsolver:latest
+# docker run --name cutsolver -it -p 8000:80 --rm cutsolver:latest
 
 # -it: interactive (debugging)
 # -d: background (normal use)
