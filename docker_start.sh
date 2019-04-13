831b529e1e7e6b45236cad3c9932a42f68723e2e
MagicManFoli <magicmanfoli@gmail.com>
2019-04-13 17:26:38 +0200

    change to docker-compose

--- docker_start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker container rm cutsolver
-docker run --name cutsolver --restart=always -d -p 80:80 cutsolver:latest
-# docker run --name cutsolver -it -p 8000:80 --rm cutsolver:latest
-
-# -it: interactive (debugging)
-# -d: background (normal use)
-# --restart=always: use when deploying
