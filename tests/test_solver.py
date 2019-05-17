a3f4570c39fc60ff6a2e70b2b7ccb42e8c623c68
ModischFabrications <modisch.fabrications@gmail.com>
2019-05-17 22:36:28 +0200

    extract Job tests

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -54,28 +54,6 @@ def test_FFD():
     assert cmp_job == job
 
 
-def test_job_generator():
-    job = Job(length_stock=1550, target_sizes=(
-        TargetSize(length=500, amount=4), TargetSize(length=200, amount=3), TargetSize(length=100, amount=2)),
-              cut_width=5)
-
-    resulting_list = []
-    for length in job.get_sizes():
-        resulting_list.append(length)
-
-    assert resulting_list == [500, 500, 500, 500, 200, 200, 200, 100, 100]
-
-
-def test_job_dunders():
-    job1 = Job(length_stock=100, target_sizes=(TargetSize(length=100, amount=2), TargetSize(length=200, amount=1)),
-               cut_width=0)
-    job2 = Job(length_stock=100, target_sizes=(TargetSize(length=100, amount=2), TargetSize(length=200, amount=1)),
-               cut_width=0)
-
-    assert job1 == job2
-    assert len(job1) == 3
-
-
 def test_full_model():
     json_file = Path("./tests/testjob.json")
     assert json_file.exists()
