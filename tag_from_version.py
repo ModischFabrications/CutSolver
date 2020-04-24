4222d8b5318214774def0bfc319432dd3d0d1379
ModischFabrications <modisch.fabrications@gmail.com>
2020-04-24 21:52:02 +0200

    autolabel git versions, closes #38

--- tag_from_version.py
+++ tag_from_version.py
@@ -12,24 +12,24 @@ def process():
     assert not repo.bare
 
     version = main.version
-    print(f"code version: {version}")
     newest_tag = repo.tags[-1]
     newest_git_version = newest_tag.name
-    print(f"git version: {newest_git_version}")
+    print(f"code version: {version}, git version: {newest_git_version}")
 
     if newest_git_version == version:
         print("Everything up to date, job done")
         return 0
 
     if newest_git_version > version:
-        print("You updated git without updating the code version!", file=stderr)
+        print("Git was updated without updating the code version!", file=stderr)
         # TODO: edit code (this is very dangerous!)
         return 1
 
     if newest_git_version < version:
-        print("git version outdated, updating with newest tag")
-        # FIXME: tag with new version
-        # repo.create_tag()
+        print("git tag outdated, updating with newest version")
+        # this could technically go back in history until the version change happened,
+        #  but as long as nothing was pushed we can skip local only versions.
+        repo.create_tag(version, message=f"autobump version from {newest_git_version} to {version}")
 
 
 if __name__ == '__main__':
