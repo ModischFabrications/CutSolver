caf4d1f994bf25b7ceed6a720f29d449f01b625e
Bouni <bouni@owee.de>
2024-01-19 10:18:34 +0100

    Refactor Result to output langth and name

--- app/solver/data/Result.py
+++ app/solver/data/Result.py
@@ -1,5 +1,5 @@
 from enum import unique, Enum
-from typing import List
+from typing import List, Tuple
 
 from pydantic import BaseModel
 
@@ -18,23 +18,23 @@ class Result(BaseModel):
     job: Job
     solver_type: SolverType
     time_us: int = -1
-    lengths: List[List[int]]
+    lengths: List[List[Tuple[int, str]]]
 
     # no trimmings as they can be inferred from difference to job
 
     def __eq__(self, other):
         return (
-                self.job == other.job
-                and self.solver_type == other.solver_type
-                and self.lengths == other.lengths
+            self.job == other.job
+            and self.solver_type == other.solver_type
+            and self.lengths == other.lengths
         )
 
     def exactly(self, other):
         return (
-                self.job == other.job
-                and self.solver_type == other.solver_type
-                and self.time_us == other.time_us
-                and self.lengths == other.lengths
+            self.job == other.job
+            and self.solver_type == other.solver_type
+            and self.time_us == other.time_us
+            and self.lengths == other.lengths
         )
 
     def assert_valid(self):
--- tests/solver/data/test_Result.py
+++ tests/solver/data/test_Result.py
@@ -6,34 +6,162 @@ from tests.test_utils import generate_testjob
 
 def test_constructor():
     job = generate_testjob()
-    result = Result(job=job, solver_type=SolverType.FFD, time_us=100, lengths=[(100, 100, 100), (200, 200, 200)])
+    result = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=100,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
     assert result
 
 
 def test_valid():
     job = generate_testjob()
-    result = Result(job=job, solver_type=SolverType.FFD, time_us=100, lengths=[(100, 100, 100), (200, 200, 200)])
+    result = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=100,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
     result.assert_valid()
 
 
 def test_invalid():
     job = generate_testjob()
-    invalid_result = Result(job=job, solver_type=SolverType.FFD, time_us=-1, lengths=[(100, 100, 100), (200, 200, 200)])
+    invalid_result = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=-1,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
     with pytest.raises(ValueError):
         invalid_result.assert_valid()
 
 
 def test_equal():
     job = generate_testjob()
-    result1 = Result(job=job, solver_type=SolverType.FFD, time_us=100, lengths=[(100, 100, 100), (200, 200, 200)])
-    result2 = Result(job=job, solver_type=SolverType.FFD, time_us=200, lengths=[(100, 100, 100), (200, 200, 200)])
+    result1 = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=100,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
+    result2 = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=200,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
     assert result1 == result2
 
 
 def test_exactly():
     job = generate_testjob()
-    result1 = Result(job=job, solver_type=SolverType.FFD, time_us=100, lengths=[(100, 100, 100), (200, 200, 200)])
-    result2 = Result(job=job, solver_type=SolverType.FFD, time_us=200, lengths=[(100, 100, 100), (200, 200, 200)])
-    result3 = Result(job=job, solver_type=SolverType.FFD, time_us=200, lengths=[(100, 100, 100), (200, 200, 200)])
+    result1 = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=100,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
+    result2 = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=200,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
+    result3 = Result(
+        job=job,
+        solver_type=SolverType.FFD,
+        time_us=200,
+        lengths=[
+            [
+                (100, "Part1"),
+                (100, "Part1"),
+                (100, "Part1"),
+            ],
+            [
+                (200, "Part2"),
+                (200, "Part2"),
+                (200, "Part2"),
+            ],
+        ],
+    )
     assert not result1.exactly(result2)
     assert result2.exactly(result3)
