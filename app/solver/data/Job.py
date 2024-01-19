04672abfa3e20e356640e502a242f6ef352ba202
Bouni <bouni@owee.de>
2024-01-19 10:17:34 +0100

    Refactor Job to take a length and a name as input

--- app/solver/data/Job.py
+++ app/solver/data/Job.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Dict, List
+from typing import Iterator, List, Optional, Tuple
 
 from pydantic import BaseModel
 
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class TargetSize(BaseModel):
     length: int
     quantity: int
+    name: Optional[str] = ""
 
     def __lt__(self, other):
         """
@@ -20,38 +21,40 @@ class TargetSize(BaseModel):
 class Job(BaseModel):
     max_length: int
     cut_width: int = 0
-    target_sizes: Dict[int, int]
+    target_sizes: List[TargetSize]
 
     # utility
 
-    def iterate_sizes(self) -> Iterator[int]:
+    def iterate_sizes(self) -> Iterator[Tuple[int, str | None]]:
         """
         yields all lengths, sorted descending
         """
 
         # sort descending to favor combining larger sizes first
-        for size, quantity in sorted(self.target_sizes.items(), reverse=True):
-            for _ in range(quantity):
-                yield size
+        for target in sorted(self.target_sizes, key=lambda x: x.length, reverse=True):
+            for _ in range(target.quantity):
+                yield (target.length, target.name)
 
+    # NOTE: Not used, so not really refactored at the moment
     def sizes_from_list(self, sizes_list: List[TargetSize]):
-        known_sizes = {}
+        # known_sizes = {}
+        #
+        # # list to dict to make them unique
+        # for size in sizes_list:
+        #     if size.length in known_sizes:
+        #         known_sizes[size.length] += size.quantity
+        #     else:
+        #         known_sizes[size.length] = size.quantity
 
-        # list to dict to make them unique
-        for size in sizes_list:
-            if size.length in known_sizes:
-                known_sizes[size.length] += size.quantity
-            else:
-                known_sizes[size.length] = size.quantity
+        self.target_sizes = sizes_list
 
-        self.target_sizes = known_sizes
-
+    # NOTE: Can eventually be removed as it does nothing anymore
     def sizes_as_list(self) -> List[TargetSize]:
         """
         Compatibility function
         """
         # back to list again for compatibility
-        return [TargetSize(length=l, quantity=q) for (l, q) in self.target_sizes.items()]
+        return self.target_sizes
 
     def assert_valid(self):
         if self.max_length <= 0:
@@ -59,20 +62,25 @@ class Job(BaseModel):
         if self.cut_width < 0:
             raise ValueError(f"Job has invalid cut_width {self.cut_width}")
         if len(self.target_sizes) <= 0:
-            raise ValueError(f"Job is missing target_sizes")
-        if any(size > (self.max_length - self.cut_width) for size in self.target_sizes.keys()):
-            raise ValueError(f"Job has target sizes longer than the stock")
+            raise ValueError("Job is missing target_sizes")
+        if any(
+            target.length > (self.max_length - self.cut_width)
+            for target in self.target_sizes
+        ):
+            raise ValueError("Job has target sizes longer than the stock")
 
     def __len__(self) -> int:
         """
         Number of target sizes in job
         """
-        return sum(self.target_sizes.values())
+        return sum([target.quantity for target in self.target_sizes])
 
     def __eq__(self, other):
-        return self.max_length == other.max_length and \
-               self.cut_width == other.cut_width and \
-               self.target_sizes == other.target_sizes
+        return (
+            self.max_length == other.max_length
+            and self.cut_width == other.cut_width
+            and self.target_sizes == other.target_sizes
+        )
 
     def __hash__(self) -> int:
-        return hash((self.max_length, self.cut_width, str(sorted(self.target_sizes.items()))))
+        return hash((self.max_length, self.cut_width, str(sorted(self.target_sizes))))
