395bfaea660de33b0185c100d40a762429a98756
Bouni <bouni@owee.de>
2024-01-19 10:18:59 +0100

    Refactor json tests

--- tests/solver/data/test_json.py
+++ tests/solver/data/test_json.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 
 # import from app! Modules won't be found otherwise
-from app.solver.data.Job import Job
+from app.solver.data.Job import Job, TargetSize
 
 
 def test_to_json():
-    job = Job(max_length=1200, cut_width=5, target_sizes={"300": 4, "200": 3})
-    assert job.json() == '{"max_length": 1200, "cut_width": 5, ' \
-                         '"target_sizes": {"300": 4, "200": 3}}'
+    job = Job(
+        max_length=1200,
+        cut_width=5,
+        target_sizes=[
+            TargetSize(length=300, quantity=4, name="Part1"),
+            TargetSize(length=200, quantity=3),
+        ],
+    )
+    assert (
+        job.model_dump_json()
+        == '{"max_length":1200,"cut_width":5,"target_sizes":[{"length":300,"quantity":4,"name":"Part1"},{"length":200,"quantity":3,"name":""}]}'
+    )
 
 
 def test_from_json():
@@ -15,6 +24,6 @@ def test_from_json():
     assert json_file.exists()
 
     with open(json_file, "r") as encoded_job:
-        job = Job.parse_raw(encoded_job.read())
+        job = Job.model_validate_json(encoded_job.read())
         assert job.__class__ == Job
         assert len(job) > 0
